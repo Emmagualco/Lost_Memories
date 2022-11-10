fb842835d755d644fd93c96a009630b14a530644
Emmagualco <emmagualco@gmail.com>
2022-11-10 18:59:27 -0300

    fixing

--- Assets/Scripts/Player/Cameras.cs
+++ Assets/Scripts/Player/Cameras.cs
@@ -7,23 +7,18 @@ namespace EG
 {
     public GameObject camOne;
     public GameObject camTwo;
-      [SerializeField]
+        [SerializeField]
+        float mouseSensitivity = 2.5f;
+        [SerializeField]
+        [Range(0.0f, 0.5f)]
+        float mouseSmoothTime = 0.03f;
+        [SerializeField]
     Transform playerCamera;
-    //[SerializeField]
-    //float mouseSensitivity = 2.5f;
-    //[SerializeField]
-    //[Range(0.0f, 0.5f)]
-    //float mouseSmoothTime = 0.03f;
-    //[SerializeField]
-    //[Range(0.0f, 0.5f)]
-    //float moveSmoothTime = 0.3f;
-    //[SerializeField]
-    //bool lockCursor = true;
-    //Vector2 currentMouseDelta = Vector2.zero;
-    //Vector2 currentMouseDeltaVelocity = Vector2.zero;
+        float cameraPitch = 0.0f;
+        Vector2 currentMouseDelta = Vector2.zero;
+        Vector2 currentMouseDeltaVelocity = Vector2.zero;
 
-    //float cameraPitch = 0.0f;
-    void Start()
+        void Start()
     {
         camOne.SetActive(true);
         camTwo.SetActive(false);
@@ -34,48 +29,50 @@ namespace EG
     void Update()
     {
         ChangeCamera();
-        //UpdateMouseLook();
-            //Look();
+           
     }
-    //void UpdateMouseLook()
-    //{
-    //    Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-    //    targetMouseDelta.Normalize();
-
-    //    currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
-
-    //    cameraPitch -= currentMouseDelta.y * mouseSensitivity;
-
-
-    //    cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
-
-    //    playerCamera.localEulerAngles = Vector3.right * cameraPitch;
-
-    //    transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
-    //}
-        //private void Look()
-        //{
-        //    transform.Rotate(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
-        //}
-        void ChangeCamera()
+ 
+   
+    void ChangeCamera()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
            
-            if (camOne.activeInHierarchy)
+            if (camTwo.activeInHierarchy)
             {
+                    camOne.SetActive(true);
+                    camTwo.SetActive(false);
+                    UpdateMouseLook();
 
-                camOne.SetActive(false);
-                camTwo.SetActive(true);
-                
+
             }
             else
             {
-                camOne.SetActive(true);
-                camTwo.SetActive(false);
-               
-            }
+
+                    camOne.SetActive(false);
+                    camTwo.SetActive(true);
+
+
+
+                }
         }
     }
-}
+        void UpdateMouseLook()
+        {
+            Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            targetMouseDelta.Normalize();
+
+            currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+
+            cameraPitch -= currentMouseDelta.y * mouseSensitivity;
+
+
+            cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
+
+            playerCamera.localEulerAngles = Vector3.right * cameraPitch;
+
+            transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+        }
+
+    }
 }
--- Assets/Scripts/Player/CharacterControl.cs
+++ Assets/Scripts/Player/CharacterControl.cs
@@ -12,16 +12,17 @@ namespace EG
 
         [Header("Stats")]
         [SerializeField] private float speed = 3f;
-        float movementSpeed = 0;
         [SerializeField] private float runSpeed = 10f;
-        [SerializeField] private float jumpForce = 8f;
+        [SerializeField] private float jumpForce = 0.1f;
         [SerializeField] private float spinTime = 0.1f;
         float speedSpin;
         [Header("Ground Data")]
         [SerializeField] private Transform detectGround;
         [SerializeField] private float distanceGround;
         [SerializeField] private LayerMask layerGround;
-
+        private Transform elementstaff;
+        public GameObject fireballPrefab;
+        private GameObject currentFireball;
         public float horizontal = 0.0f;
         public float vertical = 0.0f;
         float gravity = -9.81f;
@@ -32,9 +33,18 @@ namespace EG
         public bool die = false;
         public bool dead = false;
         public bool superattack = false;
-        private float rSpeed = 3.0f;
-        private float X = 0.0f;
-        private float Y = 0.0f;
+        public GameObject efecto_sangre;
+        public GameObject efecto_impacto;
+        public ParticleSystem flashEffect;
+        public ParticleSystem efectoCasquillo;
+        public float range = 100f;
+
+
+        public AudioSource _audSource;
+
+        public AudioClip clip_disparo;
+        public AudioClip clip_hit;
+
 
         // Start is called before the first frame update
         void Start()
@@ -55,7 +65,7 @@ namespace EG
         {
 
             DamageManage();
-
+            Attack();
 
         }
 
@@ -64,6 +74,82 @@ namespace EG
             canJump = false;
             animator.SetBool("touchFloor", false);
             animator.SetBool("Jump", false);
+        }
+        void Disparar()
+        {
+            flashEffect.Play();
+            efectoCasquillo.Play();
+
+            PonerPlaySonido(clip_disparo);
+
+            RaycastHit hit;
+            if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+            {
+                if (hit.transform.CompareTag("Enemy"))
+                {
+                    
+                    PonerPlaySonido(clip_hit);
+                    GameObject go = Instantiate(efecto_sangre, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(go, 3f);
+
+                    GameObject goImpact = Instantiate(efecto_impacto, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(goImpact, 3f);
+                }
+                if (hit.transform.CompareTag("Enemy2"))
+                {
+                    //Enemigo.health -= 30;
+                    PonerPlaySonido(clip_hit);
+                    GameObject go = Instantiate(efecto_sangre, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(go, 3f);
+
+                    GameObject goImpact = Instantiate(efecto_impacto, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(goImpact, 3f);
+                }
+                if (hit.transform.CompareTag("Wall"))
+                {
+
+                    PonerPlaySonido(clip_hit);
+                    GameObject go = Instantiate(efecto_sangre, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(go, 3f);
+
+                    GameObject goImpact = Instantiate(efecto_impacto, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(goImpact, 3f);
+                }
+            }
+        }
+        void Attack()
+        {
+
+            float fireSpeed = 200.0f;
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                attack = true;
+                Disparar();
+                currentFireball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                currentFireball.GetComponent<Rigidbody>().AddForce(transform.forward * fireSpeed,
+                                                                   ForceMode.Impulse);
+                currentFireball.gameObject.transform.parent = null;
+                currentFireball.GetComponent<AudioSource>().volume = GameMaster.sharedInstance.sfxVolume;
+                currentFireball.GetComponent<AudioSource>().Play();
+                animator.SetBool("Attack", true);
+                Invoke("LoadNewFireball", 1.0f);
+
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                attack = false;
+                animator.SetBool("Attack", false);
+            }
+          
+
+            if (Input.GetMouseButton(1))
+            {
+                superattack = true;
+
+
+            }
+
         }
         void Movement()
         {
@@ -95,19 +181,18 @@ namespace EG
             }
             animator.SetFloat("Vertical", vertical);
             animator.SetFloat("Horizontal", horizontal);
-            if (canJump)
+            if (Input.GetButtonDown("Jump") )
             {
-                if (touchFloor && velocity.y < 0)
-                {
-                    velocity.y = -2f;
-                }
-                if (Input.GetKeyDown(KeyCode.Space))
-                {
+                //if (touchFloor && velocity.y < 0)
+                //{
+                //    velocity.y = -2f;
+                //}
+                canJump = true;
                     animator.SetBool("Jump", true);
-                    //rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                    rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
                     velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
-                }
-                animator.SetBool("touchFloor", true);
+               
+                //animator.SetBool("touchFloor", true);
             }
             else
             {
@@ -143,8 +228,19 @@ namespace EG
 
 
 
-        }       
-
+        }
+        private void LoadNewFireball()
+        {
+            currentFireball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            currentFireball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            currentFireball.transform.position = elementstaff.position;
+            currentFireball.gameObject.transform.parent = elementstaff;
+        }
+        void PonerPlaySonido(AudioClip clip)
+        {
+            _audSource.clip = clip;
+            _audSource.Play();
+        }
 
     }
 }
